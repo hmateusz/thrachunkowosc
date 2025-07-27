f431c58b345c43ad10b7c61e77a651705d9f265d
hmateusz <mateusz.hajduczek@dazn.com>
2025-07-27 18:12:33 +0200

    fix

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/4.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 4;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/contact/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/4.CKj2mh8G.js","_app/immutable/chunks/BonEYqnS.js","_app/immutable/chunks/D0QH3NT1.js","_app/immutable/chunks/DtOkw9C0.js","_app/immutable/chunks/IHki7fMi.js","_app/immutable/chunks/DQJN6VY2.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/6.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 6;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/services/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/6.CeX1Rt4E.js","_app/immutable/chunks/BonEYqnS.js","_app/immutable/chunks/D0QH3NT1.js","_app/immutable/chunks/DtOkw9C0.js","_app/immutable/chunks/IHki7fMi.js","_app/immutable/chunks/DQJN6VY2.js"];
-export const stylesheets = [];
-export const fonts = [];
